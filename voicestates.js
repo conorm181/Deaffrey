4409dc56c364a565725ec6c6df148c51f9a45645
conorm181 <60738319+conorm181@users.noreply.github.com>
2022-05-14 12:15:12 +0100

    Updated file accordingly

--- voicestates.js
+++ voicestates.js
@@ -1,4 +1,5 @@
 var moment = require('moment');
+var file = require('./file.js');
 var vol = {};
 module.exports = {
     detectDeafen: function(oldState, newState) {
@@ -23,6 +24,7 @@ module.exports = {
         //var diffInMinutes = date2.diff(date1, 'minutes');
         let temp = new moment().diff(vol[oldState.member.user.id].time, 'seconds');
             console.log(oldState.member.user.username + " has undeafened! after "+ temp +"\n\n\n");
+            file.writeToFile(oldState.member.user.id,temp);
             //TODO check for new leader
             //TODO timespan diff for logging
         }
