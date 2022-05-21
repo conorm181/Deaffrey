68fc0ef96b4d5ca93595e24dc415a3e7b4ba110e
conorm181 <60738319+conorm181@users.noreply.github.com>
2022-05-21 13:27:59 +0100

    new code to detect edge cases

--- voicestates.js
+++ voicestates.js
@@ -3,31 +3,48 @@ var file = require('./file.js');
 var vol = {};
 module.exports = {
     detectDeafen: function(oldState, newState) {
-        /*
-        Debug Block
-        console.log('-----------------------------------OldState----------------------------------------');
-        console.log(oldState.id+' - '+oldState.selfDeaf);
-        console.log('-----------------------------------NewState----------------------------------------');
-        console.log(newState.id+' - '+newState.selfDeaf+'\n\n\n\n');
-        */
 
-
-        if(!oldState.selfDeaf && newState.selfDeaf){
+        //Join Deafen/Leave Deafen
+        if(((oldState.channelId != newState.channelId) && newState.selfDeaf)){
+            if(vol == undefined)
+                vol = {};
+            if(vol[newState.member.user.id]==undefined || vol[newState.member.user.id]== null)
+            {
+                console.log("User Joined the channel deafened\n");
+                vol[oldState.member.user.id.toString()] = {time:new moment()};
+                console.log(oldState.member.user.username + " has deafened! at "+ vol[oldState.member.user.id].time +"\n\n\n");
+                
+            }
+            else
+            {
+                console.log("User left the channel while deaf");
+                let temp = new moment().diff(vol[oldState.member.user.id].time, 'seconds');
+                console.log(oldState.member.user.username + " has undeafened! after "+ temp +"\n\n\n");
+                file.writeToFile(oldState.member.user.id,temp);
+                vol[newState.member.user.id] = null;
+                
+            }
+        }//Deafen
+        else if(!oldState.selfDeaf && newState.selfDeaf){
+            console.log("User deafened in channel");
             if(vol == undefined)
                 vol = {};
             vol[oldState.member.user.id.toString()] = {time:new moment()};
             console.log(oldState.member.user.username + " has deafened! at "+ vol[oldState.member.user.id].time +"\n\n\n");
             
-
-        }
+        }//Undeafen/Leave Deafen
         else if(oldState.selfDeaf && !newState.selfDeaf){
-        //var diffInMinutes = date2.diff(date1, 'minutes');
-        let temp = new moment().diff(vol[oldState.member.user.id].time, 'seconds');
+            console.log("The user undeafened in channel");
+            let temp = new moment().diff(vol[oldState.member.user.id].time, 'seconds');
             console.log(oldState.member.user.username + " has undeafened! after "+ temp +"\n\n\n");
             file.writeToFile(oldState.member.user.id,temp);
-            //TODO check for new leader
-            //TODO timespan diff for logging
+            vol[oldState.member.user.id] = null;
+            
         }
+
+
+
+
         }
    
 };
