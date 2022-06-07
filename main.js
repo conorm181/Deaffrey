0bd9c48441fca635b6121a527da1e1061799d321
conorm181 <conorm18@gmail.com>
2022-06-07 15:43:29 +0100

    code clean up

--- main.js
+++ main.js
@@ -5,27 +5,9 @@ const { Client, Intents, Guild } = require('discord.js');
 // Instantiate a new client with some necessary parameters.
 const client = new Client({ 
         intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, "GUILD_MEMBERS"] 
-        
     }
 );
-/*import fetch from 'node-fetch';
-fetch("https://w2g.tv/rooms/create.json", {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        "w2g_api_key": process.env.W2G_TOKEN,
-        "share": "https://www.youtube.com/watch?v=8Wdp35Z-fRs",
-        "bg_color": "#00ff00",
-        "bg_opacity": "50"
-    })
-})
-.then(response => response.json())
-.then(function (data) {
-    console.log("W2G: Here is your room! \n https://w2g.tv/rooms/" + data.streamkey);
-});*/
+
 //Accessing the libraries
 var clientMessage = require('./message.js');
 var vs = require('./voicestates.js');
@@ -37,36 +19,30 @@ var util = require('util');
 var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
 var log_stdout = process.stdout;
 console.log = function(d) { //
-  log_file.write(util.format(d) + '\n');
-  log_stdout.write(util.format(d) + '\n');
+  log_file.write(util.format.apply(null, arguments) + '\n');
+  log_stdout.write(util.format.apply(null, arguments) + '\n');
 };
+console.error = console.log;
 var output = "";
 file.readFile();
-//file.getLeaderboard();
+
 // Notify progress
 client.on('ready', function(e){
-  console.log(`Logged in as ${client.user.tag}!`);
-
-  let voiceLogs = {};
-
-const Guilds = client.guilds.cache.map(guild => guild.id);
-            //console.log(Guilds);
-            
+    console.log(`Logged in as ${client.user.tag}!`);
+    const Guilds = client.guilds.cache.map(guild => guild.id);
             let fuck ="";
             const guild = client.guilds.resolve(Guilds[0]);
-            console.log(guild);
             // Fetch the members of the guild and log them
             const x = guild.members.fetch()
                 .then(
                     members => {
                         members.forEach(member => {
                             output+=member.user.id + "," + member.user.username + "\n";
-                            //console.log(member.user.username);
                         });
                     }
-                )
-                .catch(console.error);
+                ).catch(console.error);
 })
+
 // Authenticate
 client.login(process.env.DISCORD_TOKEN)
 
--- voicestates.js
+++ voicestates.js
@@ -3,6 +3,7 @@ var file = require('./file.js');
 var vol = {};
 module.exports = {
     detectDeafen: function(oldState, newState) {
+        try{
 
         //Join Deafen/Leave Deafen
         if(((oldState.channelId != newState.channelId) && newState.selfDeaf)){
@@ -10,40 +11,43 @@ module.exports = {
                 vol = {};
             if(vol[newState.member.user.id]==undefined || vol[newState.member.user.id]== null)
             {
-                console.log("User Joined the channel deafened\n");
+                //console.log("User Joined the channel deafened\n");
                 vol[oldState.member.user.id.toString()] = {time:new moment()};
-                console.log(oldState.member.user.username + " has deafened! at "+ vol[oldState.member.user.id].time +"\n\n\n");
+                console.log(oldState.member.user.username + " has deafened! at "+ vol[oldState.member.user.id].time);
                 
             }
             else
             {
-                console.log("User left the channel while deaf");
+                //console.log("User left the channel while deaf");
                 let temp = new moment().diff(vol[oldState.member.user.id].time, 'seconds');
-                console.log(oldState.member.user.username + " has undeafened! after "+ temp +"\n\n\n");
+                console.log(oldState.member.user.username + " has undeafened! after "+ temp);
                 file.writeToFile(oldState.member.user.id,temp, oldState.member.user.username);
                 vol[newState.member.user.id] = null;
                 
             }
         }//Deafen
         else if(!oldState.selfDeaf && newState.selfDeaf){
-            console.log("User deafened in channel");
+            //console.log("User deafened in channel");
             if(vol == undefined)
                 vol = {};
             vol[oldState.member.user.id.toString()] = {time:new moment()};
-            console.log(oldState.member.user.username + " has deafened! at "+ vol[oldState.member.user.id].time +"\n\n\n");
+            console.log(oldState.member.user.username + " has deafened! at "+ vol[oldState.member.user.id].time);
             
         }//Undeafen/Leave Deafen
         else if(oldState.selfDeaf && !newState.selfDeaf){
-            console.log("The user undeafened in channel");
+            //console.log("The user undeafened in channel");
             let temp = new moment().diff(vol[oldState.member.user.id].time, 'seconds');
-            console.log(oldState.member.user.username + " has undeafened! after "+ temp +"\n\n\n");
+            console.log(oldState.member.user.username + " has undeafened! after "+ temp);
             file.writeToFile(oldState.member.user.id,temp, oldState.member.user.username);
             vol[oldState.member.user.id] = null;
             
         }
 
 
-
+    }
+    catch(e){
+        console.log(e);
+    }
 
         }
    
